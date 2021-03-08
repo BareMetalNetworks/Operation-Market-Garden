95613f4e71fd411ce80ef938d92dd178bdd6d2c3
Samuel Kerr <transiencymail@gmail.com>
2021-03-08 07:45:09 -0700

    added crop details to model

--- app/db/migrate/20210307162225_create_plants.rb
+++ app/db/migrate/20210307162225_create_plants.rb
@@ -10,7 +10,32 @@ class CreatePlants < ActiveRecord::Migration[6.1]
       t.text :notes
       t.float :price
       t.string :unit
-
+      t.string :sow
+      t.string :frost_tolerance
+      t.string :sow
+      t.string :disease_resistance
+      t.string :weed_resistance
+      t.string :pest_resistence
+      t.string :description
+      t.integer :zone
+      t.integer :base_temperature
+      t.integer :germination
+      t.string :plant_spacing
+      t.string :planting_depth
+      t.string :watering
+      t.text :fertilization_strategy
+      t.string :companion_plants
+      t.string :contraindicted_plants
+      t.string :pests
+      t.string :diseases
+      t.text :harvesting
+      t.text :storing
+      t.string :botanical_name
+      t.string :origin
+      t.string :image_url
+      t.string :height
+      t.string :best_air_temperatures
+      
       t.timestamps
     end
   end
